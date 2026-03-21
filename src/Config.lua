3c4abd33872355ec5b3941264d3bd5b2aab05a17
DevSourcererScripts <marcouriel776@gmail.com>
2026-03-21 16:04:15 -0600

    Update configuration values in Config.lua
    Reduced ChaosSpeed and removed some configuration options.

--- src/Config.lua
+++ src/Config.lua
@@ -1,9 +1,6 @@
 return {
 	SoundId = "rbxassetid://7266001792",
-	MaxWindows = 6,
 	MaxAttempts = 10,
-	ChaosSpeed = 0.2,
-	TitleText = "YOU ARE AN IDIOT 😈",
+	ChaosSpeed = 0.12,
 	FinalText = "Demasiado tarde...",
-	InitialWindows = 3,
 }
