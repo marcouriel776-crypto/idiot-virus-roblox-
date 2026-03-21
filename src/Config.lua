73b6b97a53e612e8835f3477d624c2b542a64b0a
DevSourcererScripts <marcouriel776@gmail.com>
2026-03-21 15:29:55 -0600

    Add configuration file for game settings

---
+++ src/Config.lua
@@ -0,0 +1,9 @@
+return {
+	SoundId = "rbxassetid://7266001792",
+	MaxWindows = 6,
+	MaxAttempts = 10,
+	ChaosSpeed = 0.2,
+	TitleText = "YOU ARE AN IDIOT 😈",
+	FinalText = "Demasiado tarde...",
+	InitialWindows = 3,
+}
